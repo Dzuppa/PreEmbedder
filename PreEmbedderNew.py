bfc2dbcd11353ebea00ae2118eacb439ee685eca
Fabio Massimo Zanzotto <fabio.massimo.zanzotto@uniroma2.it>
2017-04-12 16:00:50 +0200

    A better model

--- PreEmbedderNew.py
+++ PreEmbedderNew.py
@@ -1,15 +1,18 @@
 
-
+import random
 
 class PreEmbedderNew:
 
-    def __init__(self,  function, Xs_with_trees, Xs_additiona_features = None,  Ys, batchDim = 50):
-       self._Ys = Ys
-       self._Xs_with_trees = Xs_with_trees
-       self._Xs_additiona_features = Xs_additiona_features
-       self._function = function
-       self._batchDim = batchDim
-       self._batchIndex = 0
+    def __init__(self,  function, Xs_with_trees, Xs_additional_features = None,  Ys, batchDim = 50):
+        self._Ys = Ys
+        self._Xs_with_trees = Xs_with_trees
+        self._Xs_additional_features = Xs_additional_features
+        self._function = function
+        self._batchDim = batchDim
+        self._batchIndex = 0
+        self._listOfIndexes = range(len(Xs_with_trees))
+        random.shuffle(self._listOfIndexes)
+
 
 
     def hasNextBatch(self):
@@ -18,14 +21,17 @@ class PreEmbedderNew:
     def nextBacth(self):
         batchX = []
         batchY = []
+        max = self._batchIndex * (self._batchDim + 1) if self._batchIndex * (self._batchDim + 1) < len(self._listOfIndexes) else len(self._listOfIndexes) - 1
 
-        # Fill and embed batchX and Y
-
+        for i in self._listOfIndexes[self._batchIndex * self._batchDim:max] :
+            #transform X_i
+            batchX.append(self._function.embed(self._Xs_with_trees[i]) + self._Xs_additional_features[i])
+            batchY.append(self._Ys[i])
         self._batchIndex = self._batchIndex + 1
         return batchX,batchY
 
     def reset(self):
         #reshaffle examples
-
+        random.shuffle(self._listOfIndexes)
         #restart indexing
         self._batchIndex = 0

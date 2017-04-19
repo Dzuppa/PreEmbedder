d94c1d0fe874d6d9b6cde97a404919d8c3c3e985
Dzuppa <zuppellod@yahoo.it>
2017-04-19 16:13:09 +0200

    PreEmbedderNew little changes, primoEsperimentoNew.py first experiment with PreEmbedderNew

--- PreEmbedderNew.py
+++ PreEmbedderNew.py
@@ -1,31 +1,37 @@
 
 import random
+from keras_dt import *
+import numpy as np
+
+cache = {}
 
 class PreEmbedderNew:
 
-    def __init__(self,  function, Xs_with_trees, Xs_additional_features = None,  Ys, batchDim = 50):
+    def __init__(self,  function, Xs_with_trees, Ys, Xs_additional_features = None, batchDim = 50):
         self._Ys = Ys
         self._Xs_with_trees = Xs_with_trees
         self._Xs_additional_features = Xs_additional_features
         self._function = function
         self._batchDim = batchDim
         self._batchIndex = 0
-        self._listOfIndexes = range(len(Xs_with_trees))
+        self._listOfIndexes = list(range(len(Xs_with_trees)))
+        self.dt = DT(dim=32, lexicalized=True)
         random.shuffle(self._listOfIndexes)
 
 
 
     def hasNextBatch(self):
-        return len(self._Xargs) > self._batchIndex * self._batchDim
+        return len(self._Xs_with_trees) > self._batchIndex * self._batchDim
 
     def nextBacth(self):
         batchX = []
         batchY = []
-        max = self._batchIndex * (self._batchDim + 1) if self._batchIndex * (self._batchDim + 1) < len(self._listOfIndexes) else len(self._listOfIndexes) - 1
-
+        max = (self._batchIndex + 1) * (self._batchDim) if (self._batchIndex + 1) * (self._batchDim) < len(self._listOfIndexes) else len(self._listOfIndexes) - 1
+        print("max: ", max)
+        print("list 4 for: ", self._listOfIndexes[self._batchIndex * self._batchDim:max])
         for i in self._listOfIndexes[self._batchIndex * self._batchDim:max] :
             #transform X_i
-            batchX.append(self._function.embed(self._Xs_with_trees[i]) + self._Xs_additional_features[i])
+            batchX.append(np.concatenate([cachesBlocksFromBlockPair(self._function)(self.dt,self._Xs_with_trees[i]), self._Xs_additional_features[i]], axis=0))
             batchY.append(self._Ys[i])
         self._batchIndex = self._batchIndex + 1
         return batchX,batchY
@@ -35,3 +41,34 @@ class PreEmbedderNew:
         random.shuffle(self._listOfIndexes)
         #restart indexing
         self._batchIndex = 0
+
+
+
+#funzione per decorare la funzione che passo al preembedder che serve a cachare i risultati che ho ottenuto
+def cachesBlocksFromBlockPair(func):
+	def _func(*args):
+		x = args[-1]
+		dt = args[-2]
+		#lavorare il block per ottenere la chiave della cache assumiamo per ora che sia str(treesBlock)
+		valA = cache.get(str(x.treesA), None)  #restituisce il valore del dizionario con chiave primo argomento oppure il secondo argomento (default)
+		valB = cache.get(str(x.treesB), None)
+		if valA is not None and valB is not None:
+			print("..")
+			return np.concatenate([valA, valB])
+		elif valA is None and valB is not None:
+			valA = x.blockOneToDT(dt)
+			print(".")
+			cache[str(x.treesA)] = valA
+			return np.concatenate([valA, valB])
+		elif valA is not None and valB is None:
+			valB = x.blockTwoToDT(dt)
+			print(".")
+			cache[str(x.treesB)] = valB
+			return np.concatenate([valA, valB])
+		else: #valA is None and valB is None
+			value = func(x,dt)
+			print("calculated")
+			cache[str(x.treesA)] = value[:x.dim]
+			cache[str(x.treesB)] = value[x.dim:]
+			return value
+	return _func
---
+++ primoEsperimentoNew.py
@@ -0,0 +1,68 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+import keras.optimizers as opt
+import numpy as np
+import sys
+import corpus_reader as cr
+import PreEmbedderNew as pe
+import blockpair as bp
+from trees import *
+
+max_num_of_words = 40000
+inputs = []
+base = "../standard_split_reduced_3/"
+
+X_train,X_train_specific_features,Y_train,max_value,_,Pairs = cr.read_corpus(base+"Dev-training.txt", base+"blocks.txt", 3,n_of_words=max_num_of_words)
+'''
+X_test,X_test_specific_features,Y_test,max1,Y_test_table,PairsTest = cr.read_corpus(base+"Dev-testing_full_grouped.txt", base+"blocks.txt", 3,max_lenght_of=max_value,n_of_words=max_num_of_words)
+'''
+model = Sequential()
+
+model.add(Dense(input_dim=73,output_dim=2, activation='sigmoid'))
+
+penew = pe.PreEmbedderNew(bp.Concatenation, Pairs, Y_train, X_train_specific_features)
+
+optim = opt.adam(0.0004, 0.9, 0.999)
+print("compiling")
+model.compile(loss='categorical_crossentropy', optimizer=optim, metrics=['accuracy'])
+print("done")
+for i in range(1,500):
+	versus = 0
+	changed = 0
+	epsilon = 0.00
+	sys.stdout.flush()
+	print("i:", i)
+
+	while(penew.hasNextBatch()):
+		batchx, batchy = penew.nextBacth()
+		batchx = np.asarray(batchx)
+		batchy = np.asarray(batchy)
+		model.fit(batchx, batchy, nb_epoch=1, batch_size=50,class_weight=[0.999,0.001],verbose=1)
+	penew.reset()
+	
+'''
+	system_predictions = final_model.predict(inputs_test[i],targets_test[i], batch_size=300, verbose=1)
+	
+	for j in np.random.random_integers(0,len(system_predictions),10):
+		print(system_predictions[j])
+	print("Generating tables ", i)
+	oracle_table = e.generate_table(Y_test_table, Y_test)    
+	system_table = e.generate_table(Y_test_table, system_predictions)    
+	print("Computing performance ", i)
+	recall_at_k_res = e.recall_at_k(oracle_table, system_table, k_s = [1,2,5,10,100,1000])
+	
+	if(changed != 0):
+		versus = 0
+		for k in range(3):
+			if(recall_at_k_res[i] < last_kres[i] or (recall_at_k_res[i] - last_kres[i]) < epsilon):
+				versus = versus + 1
+			if(versus > 2):
+				K.set_value(optim.lr, 0.5 * K.get_value(optim.lr))
+				changed = 0
+	else:
+		changed = 1;
+
+	for w in range(len(recall_at_k_res)):
+		last_kres[w] = recall_at_k_res[w]	
+	'''
+    
